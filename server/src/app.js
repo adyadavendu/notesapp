e80a0a3f67e532240f1b9fc019bd927410efe638
Aditya Yadav <adityayadav190099@gmail.com>
2026-02-22 04:32:07 +0530

    Fix the CORS for production

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,10 @@ dotenv.config()
 
 const app = express()
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  }),
-)
+app.use(cors({
+  origin: "https://notesapp-eta-six.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
